142bd53f4224a92a1f9936612bb972fcdfe84b5f
Phil <113477209+phil-macrocheira@users.noreply.github.com>
2026-01-13 21:15:04 -0600

    .

--- BEAN.py
+++ BEAN.py
@@ -432,10 +432,12 @@ def get_world_records(target, players):
         elif var_name == 'Restrictions' or var_name == 'Upgrades' or var_name == 'Difficulty' or var_name == 'Cave Extensions':
             other_var_id = var["id"]
             other_id, other_data = list(var["values"]["values"].items())[0]
-            other_var_name = f" **({other_data["label"]})**"
-            # Pilot Quest is backwards??
+
+            # Temporary fix for pilot quest being backwards??
             if game == 'Pilot Quest':
                 other_id, other_data = list(var["values"]["values"].items())[1]
+
+            other_var_name = f" **({other_data["label"]})**"
         # Item Restrictions: Mini & Max
         elif var_name == 'Item Restrictions':
             other_var_id2 = var["id"]
