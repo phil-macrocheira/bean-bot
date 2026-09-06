d56947901c375485156de5a439d064807c2f26aa
Phil <113477209+phil-macrocheira@users.noreply.github.com>
2026-09-06 16:31:08 -0500

    allgames

--- BEAN.py
+++ BEAN.py
@@ -808,6 +808,26 @@ async def seedoftheday(interaction: discord.Interaction):
     response = f'The **Seed of the Day** is **{seed}**'
     await interaction.response.send_message(response)
 
+#allgames command
+@client.tree.command(name="allgames", description="See all UFO 50 game icons", guild=GUILD_ID)
+async def allgames(interaction: discord.Interaction):
+    games_list = "".join(f'{d[i]["emoji"]}' for i in range(0, 9))
+    games_list += "\n" + "".join(f'{d[i]["emoji"]}' for i in range(10, 19))
+    games_list += "\n" + "".join(f'{d[i]["emoji"]}' for i in range(20, 29))
+    games_list += "\n" + "".join(f'{d[i]["emoji"]}' for i in range(30, 39))
+    games_list += "\n" + "".join(f'{d[i]["emoji"]}' for i in range(40, 49))
+    await interaction.response.send_message(games_list)
+
+#allgames_numbered command
+@client.tree.command(name="allgames_numbered", description="See all UFO 50 game icons with numbers", guild=GUILD_ID)
+async def allgames_numbered(interaction: discord.Interaction):
+    games_list = "".join(f'{i+1}{d[i]["emoji"]}' for i in range(0, 9))
+    games_list += "\n" + "".join(f'{i+1}{d[i]["emoji"]}' for i in range(10, 19))
+    games_list += "\n" + "".join(f'{i+1}{d[i]["emoji"]}' for i in range(20, 29))
+    games_list += "\n" + "".join(f'{i+1}{d[i]["emoji"]}' for i in range(30, 39))
+    games_list += "\n" + "".join(f'{i+1}{d[i]["emoji"]}' for i in range(40, 49))
+    await interaction.response.send_message(games_list)
+
 # history command
 @client.tree.command(name="history",description="Check history text for a game", guild=GUILD_ID)
 async def gift(interaction: discord.Interaction, game: str|None, number: int|None):
